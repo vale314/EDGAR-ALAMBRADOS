dc4c72b8d1d31eaa5d7a44daabeb4882770e231c
vale314 <electrovale314@hotmail.com>
2019-08-07 12:44:45 -0500

    Templetes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NouisliderModule } from 'ng2-nouislider';
+import { FormsModule } from '@angular/forms';
+import * as $ from '../assets/js/core/jquery.min.js';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './pages/home/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NouisliderModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
