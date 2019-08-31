801fc288ea8490c78130492bd001ce09f7f91242
vale314 <electrovale314@hotmail.com>
2019-08-30 20:13:08 -0500

    End 3.3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'https://www.provedoradealambradosyproteccionesprofesionales.com/video'
+  url: 'https://www.provedoradealambradosyproteccionesprofesionales.com'
 };
